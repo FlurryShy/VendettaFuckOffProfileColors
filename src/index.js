92bc1439e276b0aea9f31bb7eb25b644372df93e
FlurryShy <84310339+FlurryShy@users.noreply.github.com>
2024-05-27 13:09:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ export function onLoad() {
 
         resp.themeColors = colors.map((c) => parseInt("0x" + c.slice(1)));
         resp.premiumType = 2;
-        resp.bio = decoded.replaceAll(globalColorRegex, "");
+        // resp.bio = decoded.replaceAll(globalColorRegex, "");
       } catch {}
     })	
   );
